100684b81c2f4719edb114ce95f3af4fac946257
Nkululeko <n.mkhatywa2019@gmail.com>
2024-03-04 07:49:13 +0200

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import About from "./pages/About"
+import AddExpense from "./pages/AddExpense"
+import Home from "./pages/Home"
+import Expenses from "./pages/Expenses"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+import Profile from "./pages/Profile"
+import VerifyUser from "./pages/VerifyUser"
+import ForgotPassword from "./pages/ForgotPassword"
+import Contact from "./pages/Contact"
+import Dashboard from "./pages/Dashboard"
+import Settings from "./pages/Settings"
 
 export default function App() {
   return (
-    <h1 className="text-red-500 text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/verify-user" element={<VerifyUser />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/add-expense" element={<AddExpense />} />
+        <Route path="/expenses" element={<Expenses />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/pages/VerifyUser.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function VerifyUser() {
+  return (
+    <div>
+      Verify User
+    </div>
+  )
+}
